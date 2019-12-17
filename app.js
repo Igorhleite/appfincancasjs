65c293e83111b2d5191a288df911cd6d12bd63a4
Igorhleite <ygor.mxl@gmail.com>
2019-12-17 16:27:31 -0300

    commit

--- app.js
+++ app.js
@@ -229,4 +229,87 @@ function cadastrarDespesa(){ //FUNÇÃO CADASTRAR DESPESAS
         
     }
 
+/*
+    function carregaListaDespesas() { //FUNÇÃO CARREGAR LISTA DE DESPESAS (ATIVA QUANDO A PÁGINA CONSULTA.HTML É ABERTA)
 
+        let despesas = Array()
+
+        despesas = bd.recuperarTodosRegistros()
+        
+        let listaDespesas = document.getElementById('listaDespesas')
+            
+        despesas.forEach(function(d) {
+            let linha = listaDespesas.insertRow()
+
+            linha.insertCell(0).innerHTML = d.dia + '/' + d.mes + '/' + d.ano
+                    // arrumando as o tipo de despesa
+            switch(d.tipo) {
+                    case '1': d.tipo = 'Alimentação'
+                        break
+                    case '2': d.tipo = 'Educação'
+                        break
+                    case '3': d.tipo = 'Lazer'
+                        break
+                    case '4': d.tipo = 'Saúde'
+                        break
+                    case '5': d.tipo = 'Transporte'
+                        break
+            }
+            linha.insertCell(1).innerHTML = d.tipo
+            linha.insertCell(2).innerHTML = d.descricao
+            linha.insertCell(3).innerHTML = d.valor
+
+            let btn = document.createElement("button")
+            btn.className = 'btn btn-danger'
+            btn.innerHTML = '<i class="fas fa-times"> </i>'
+            btn.id = 'id_despesa' + d.id
+            btn.onclick = function(){
+                
+                let id = this.id.replace ('id_despesa', '')
+                bd.remover(id)
+
+                window.location.reload()
+            }
+            linha.insertCell(4).append(btn)
+        })
+
+}
+
+function pesquisarDespesa() { //função para pesquisar as depesas
+let ano = document.getElementById('ano').value
+let mes = document.getElementById('mes').value
+let dia = document.getElementById('dia').value
+let tipo = document.getElementById('tipo').value
+let descricao = document.getElementById('descricao').value
+let valor = document.getElementById('valor').value
+
+let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor) //variavel a ser "filtrada no db.pesqusiar"
+
+let despesas = bd.pesquisar(despesa) //variavel que recebe as despesas filtradas do objeto db.pesquisar
+
+let listaDespesas = document.getElementById('listaDespesas') // preenchendo as linhas e colunas com as despesas pesquisadas
+listaDespesas.innerHTML = '' //limpando o tbody antes de preencher com elementos pesquisados
+    
+despesas.forEach(function(d) {
+    let linha = listaDespesas.insertRow()
+
+    linha.insertCell(0).innerHTML = d.dia + '/' + d.mes + '/' + d.ano
+            // arrumando as o tipo de despesa
+    switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+    }
+    linha.insertCell(1).innerHTML = d.tipo
+    linha.insertCell(2).innerHTML = d.descricao
+    linha.insertCell(3).innerHTML = d.valor
+})
+
+} */
